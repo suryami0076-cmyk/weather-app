ef67eaef87e9239afbed78ba954c904bb4e01668
Sivaramakrishnan N <shivamaya192@gmail.com>
2026-04-21 04:12:16 +0530

    Codepush

--- src/main/java/com/clt/weather/data/api/WebConfig.java
+++ src/main/java/com/clt/weather/data/api/WebConfig.java
@@ -1,3 +1,5 @@
+package com.clt.weather.data.api;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
